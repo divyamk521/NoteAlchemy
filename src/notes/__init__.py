c72f7f1da22a65e0fb34a23de0a4c0f0ad395e94
Divyamk <divyamk521@gmail.com>
2026-05-16 16:35:06 +0530

    feat(notes): add centralized exports for notes module

---
+++ src/notes/__init__.py
@@ -0,0 +1,12 @@
+from .models import LectureNotes, NotesStructure, SectionContent, GlossaryEntry, Section
+from .generator import NotesGenerator, GenerationStats
+
+__all__ = [
+    "LectureNotes",
+    "NotesStructure",
+    "SectionContent",
+    "GlossaryEntry",
+    "Section",
+    "NotesGenerator",
+    "GenerationStats",
+]
